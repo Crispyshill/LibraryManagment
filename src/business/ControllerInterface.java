f8d6a0c880957e0f7b674a25d599a4a83b8bdf84
Seth Hillsberry <seth.hillsberry@gmail.com>
2022-06-02 13:30:01 -0500

    Added addCopy and addBook to the ControllerInterface to make the methods in the SystemController accessable

--- src/business/ControllerInterface.java
+++ src/business/ControllerInterface.java
@@ -1,4 +1,5 @@
 package business;
+import business.exceptions.LibrarySystemException;
 import business.exceptions.LoginException;
 
 import java.util.List;
@@ -8,4 +9,7 @@ public interface ControllerInterface {
 	public void login(String id, String password) throws LoginException;
 	public List<String> allMemberIds();
 	public List<String> allBookIds();
+
+	public void addCopy(String isbn) throws LibrarySystemException;
+	public void addBook(String isbn, String title, int maxCheckoutLength, List<Author> authors);
 }
